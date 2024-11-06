2209cc50d355d2e1582c979f525569e8ff1d6da8
peter <jerelepeter06@gmail.com>
2024-11-06 01:53:37 -0400

    added useState hook, linked markdown parser via cdn link, linked testing api, pushed repo to cloud

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import "./index.css";
 
 function App() {
+  const [text, useText] = useState("");
+
+  // function for changing text
+
+  const textPreview = () => {
+    return useText;
+  };
+
   return (
     <>
-      <h1 className=" font-bold underline ">Working?</h1>
+      <textarea id="editor" onChange={textPreview}>
+        {text}
+      </textarea>
+      <div id="preview"></div>
     </>
   );
 }
