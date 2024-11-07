ccaca5ba3f7b0a9dab8506dc71fb9ba0fb8c1f0d
peter <jerelepeter06@gmail.com>
2024-11-07 07:36:12 -0400

    installed heroicons

--- src/App.tsx
+++ src/App.tsx
@@ -63,11 +63,7 @@ function App() {
 
   return (
     <Theme>
-      <Flex direction="column" gap="2">
-        <Text>Hello from Radix Themes :)</Text>
-        <Button>Let's go</Button>
-      </Flex>
-
+      <Button>Bookmark</Button>
       <div>
         <h1>Markdown Previewer</h1>
         <div className="markdown">
