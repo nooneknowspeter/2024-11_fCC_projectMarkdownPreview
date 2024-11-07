71ce22427064cd4a906ccb67946050a3553af55d
peter <jerelepeter06@gmail.com>
2024-11-07 06:56:59 -0400

    udated README.md fixed type error with event parameter

--- README.md
+++ README.md
@@ -6,20 +6,23 @@
 
 ## Tech Stack / Softwares
 
-- [Gsap](https://gsap.com/docs/v3)
 - [Git](https://git-scm.com/)
 - [GitHub](https://github.com/)
 - [Figma](https://www.figma.com/)
-- [Flowbite](https://flowbite.com/docs/getting-started/introduction/)
+- [heroicons](https://heroicons.com/)
 - [React](https://react.dev/)
 - [React Animated Cursor](https://www.npmjs.com/package/react-animated-cursor)
 - [ReactThreeFiber](https://r3f.docs.pmnd.rs/getting-started/introduction)
+- [ReactThreeDrei](https://drei.docs.pmnd.rs/getting-started/introduction)
 - [Netlify](https://www.netlify.com/)
 - [Nodejs](https://nodejs.org/en/)
 - [npm](https://www.npmjs.com/)
+- [Radix](https://www.radix-ui.com/themes/docs/overview/getting-started)
 - [Tailwind](https://tailwindcss.com/docs/installation)
 - [Threejs](https://threejs.org/)
 - [TypeScript](https://www.typescriptlang.org/)
 - [Vite](https://vite.dev/)
 - [Visual Studio code](https://code.visualstudio.com/)
 - [WSL_Ubuntu](https://learn.microsoft.com/en-us/windows/wsl/)
+
+## UI / UX Prototype
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,77 @@
 import { useState } from "react";
 import "./index.css";
+import ReactMarkdown from "react-markdown";
+
+// default markdown text to fullfill userstory #5
+const defaultMarkdown = `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
 
 function App() {
-  const [text, useText] = useState("");
+  const [markdownText, setMarkdownText] = useState<string>(defaultMarkdown);
 
-  // function for changing text
-
-  const textPreview = () => {
-    return useText;
+  const markdownParse = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    console.log(event);
+    setMarkdownText(event.target.value);
   };
 
   return (
     <>
-      <textarea id="editor" onChange={textPreview}>
-        {text}
-      </textarea>
-      <div id="preview"></div>
+      <div>
+        <h1>Markdown Previewer</h1>
+        <div className="markdown">
+          <textarea
+            id="editor"
+            value={markdownText}
+            onChange={markdownParse}
+          ></textarea>
+          <div id="preview">
+            <ReactMarkdown>{markdownText}</ReactMarkdown>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
